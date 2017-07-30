b296f818ab1af903c195e57dce7a67bc72b443b9
shen100 <liushen_shen@163.com>
2017-07-30 14:37:02 +0800

    1.多核并发遍历目录，性能并没有显著提高，反而增加了代码理解的复杂度，因此，改回非并发遍历 2.并发生成文件hash, 性能显著提高

--- main.go
+++ main.go
@@ -16,27 +16,47 @@ var mu sync.Mutex
 
 // Node 树中的结点
 type Node struct {
-	ParentDir  string
-	Path       string
-	Name       string
-	Size       int64
-	isDir      bool
+	ParentDir  string //父目录的路径
+	Path       string //当前结点的完整路径
+	Name       string //当前结点的文件名
+	Size       int64  //文件大小
+	IsDir      bool   //是否是目录
 	Hash       string
 }
+
+const (
+	// Success 成功
+	Success = iota
+
+	// PathNullErr 路径为空
+	PathNullErr    
+
+	// InvalidPathErr 不是有效的路径
+	InvalidPathErr
+
+	// FileNotDIR 是文件，而不是目录
+	FileNotDIR
+
+	// PermissionErr 权限错误
+	PermissionErr
+
+	// NoChildrenErr 即没有子目录，也没有文件
+	NoChildrenErr
+)
 
 func writeHash(node *Node) {
 	file, err := os.Open(node.Path)
     if err != nil {
 		fmt.Println(err.Error())
-		displayHelp()
-        return
+		displayHelpCMD()
+        os.Exit(-1)
     }
     defer file.Close()
     h := sha1.New()
     _, ioErr := io.Copy(h, file)
     if ioErr != nil {
 		fmt.Println(ioErr.Error())
-		displayHelp()
+		displayHelpCMD()
         return
 	}
 	
@@ -48,176 +68,113 @@ func writeHash(node *Node) {
 	data  := fmt.Sprintf("%s,%x,%d\n", node.Path, h.Sum(nil), node.Size)
 	
 	buf := []byte(data)  
-    fd.Write(buf)
+	mu.Lock()
+	fd.Write(buf)
+	mu.Unlock()
 }
 
 func newNode(fileInfo os.FileInfo, parentDir string) Node {
-	//fmt.Println("====> ", parentDir + string(os.PathSeparator) + fileInfo.Name())
-
 	node := Node{
 		ParentDir : parentDir,
 		Path      : parentDir + string(os.PathSeparator) + fileInfo.Name(),
 		Name      : fileInfo.Name(),
 		Size      : fileInfo.Size(),
-		isDir     : fileInfo.IsDir(),
+		IsDir     : fileInfo.IsDir(),
 		Hash      : "",
 	}
 	return node
 }
 
-func traverse(path string, filter string) { 
-	var MULTICORE int = runtime.NumCPU()
-	runtime.GOMAXPROCS(MULTICORE)
-	fmt.Println("MULTICORE", MULTICORE)
-	
+// Traverse 遍历目录
+func Traverse(path string, filter string) int {
+	if path == "" {
+		fmt.Println("root参数不能为空")
+		displayHelpCMD()
+		return PathNullErr	
+	}
 	rootDir, err := os.Stat(path)
-	
 	if err != nil {
 		fmt.Println(path + " 不是有效的目录")
-		displayHelp()
-		return
+		displayHelpCMD()
+		return InvalidPathErr
 	}
 	if !rootDir.IsDir() {
 		fmt.Println(path + " 不是目录")
-		displayHelp()
-		return
+		displayHelpCMD()
+		return FileNotDIR
 	}
+	// 遍历时，保存树中的结点
 	var stack []Node
 	files, err := ioutil.ReadDir(path)
 	if err != nil {
-
+		fmt.Println(err.Error())
+		return PermissionErr
 	}
-	for i := 0; i < len(files); i++ {
+	length := len(files)
+	if length == 0 {
+		fmt.Println(path + " 目录下即没有子目录，也没有文件")
+		displayHelpCMD()
+		return NoChildrenErr
+	}
+	for i := 0; i < length; i++ {
 		stack = append(stack, newNode(files[i], path))
 	}
-
-	var readFileTime int64
-	var writeFileTime int64
-
+	// 树的广度优先遍历
 	for len(stack) > 0 {
-		maxNum := MULTICORE
-		if maxNum > len(stack) {
-			maxNum = len(stack)
-		}
-		ch := make(chan []Node, maxNum)
-		for num := 0; num < maxNum; num++ {
-			go func(i int) {
-				node := stack[i]
-				if node.isDir {
-					mu.Lock()
-					beginTime := time.Now()
-					files, err := ioutil.ReadDir(node.Path)
-					readFileTime = readFileTime + time.Now().Sub(beginTime).Nanoseconds()
-					mu.Unlock()
-					if err != nil {
+		node := stack[0]
+		stack = stack[1:]
+		if !node.IsDir {
+			go writeHash(&node)
+		} else {
+			files, err := ioutil.ReadDir(node.Path)
+			if err != nil {
 
-					}
-					if len(files) > 0 {
-						var nodes []Node
-						for i := 0; i < len(files); i++ {
-							nodes = append(nodes, newNode(files[i], node.Path))
-						}
-						ch <- nodes
-					} else {
-						ch <- nil	
-					}
-				} else {
-					ch <- nil	
+			}
+			if len(files) > 0 {
+				for i := 0; i < len(files); i++ {
+					stack = append(stack, newNode(files[i], node.Path))
 				}
-			}(num)
-		}
-		for num := 0; num < maxNum; num++ {
-			subTree := <- ch
-			if (subTree != nil) {
-				stack = append(stack, subTree...)	
-			}
-			node := stack[num]
-			if !node.isDir {
-				beginTime := time.Now()
-				writeHash(&node)
-				writeFileTime = writeFileTime + time.Now().Sub(beginTime).Nanoseconds()
 			}
 		}
-		stack = stack[maxNum:]
-		// node := stack[0]
-		// stack = stack[1:]
 	}
-	fmt.Println("读文件总用时: ", readFileTime)
-	fmt.Println("写文件总用时: ", writeFileTime)
+	return Success
 }
 
-// func traverse(path string, filter string) {
-// 	rootDir, err := os.Stat(path)
-// 	count := 0 
-// 	if err != nil {
-// 		fmt.Println(path + " 不是有效的目录")
-// 		displayHelp()
-// 		return
-// 	}
-// 	if !rootDir.IsDir() {
-// 		fmt.Println(path + " 不是目录")
-// 		displayHelp()
-// 		return
-// 	}
-// 	var stack []Node
-// 	files, err := ioutil.ReadDir(path)
-// 	if err != nil {
-
-// 	}
-// 	for i := 0; i < len(files); i++ {
-// 		fmt.Println(12345678, files[i].Name())
-// 		stack = append(stack, newNode(files[i], path))
-// 	}
-// 	for len(stack) > 0 {
-// 		node := stack[0]
-// 		stack = stack[1:]
-// 		count++
-// 		if !node.isDir {
-// 			writeHash(&node)
-// 		} else {
-// 			files, err := ioutil.ReadDir(node.Path)
-// 			if err != nil {
-
-// 			}
-// 			if len(files) > 0 {
-// 				for i := 0; i < len(files); i++ {
-// 					stack = append(stack, newNode(files[i], node.Path))
-// 				}
-// 			}
-// 		}
-// 	}
-// 	fmt.Println("总文件数: ", count)
-// }
-
-func displayHelp() {
+func displayHelpCMD() {
 	fmt.Println("运行以下命令获得帮助")
 	fmt.Println("go run treeash help")
 }
 
-func main() {
-	reqStartTime := time.Now()
-	if len(os.Args) == 2 && os.Args[1] == "help" {
-		fmt.Println("\n参数说明:")
-		fmt.Println("-root", "目录")
-		fmt.Println("-filter", "需要过滤的目录或文件，支持通配符")
-		os.Exit(0)
-	}
+func displayHelp() {
+	fmt.Println("\n参数说明:")
+	fmt.Println("-root", "要计算hash的根目录")
+	fmt.Println("-filter", "需要过滤的目录或文件，支持通配符")
+}
 
-	root   := flag.String("root", "", "root path")
-	filter := flag.String("filter", "", "filter")
+func main() {
+	beginTime := time.Now()
+	root      := flag.String("root", "", "root path")
+	filter    := flag.String("filter", "", "filter")
+
 	flag.Parse()
 
-	fmt.Println(os.Args, *root, *filter)
+	args := flag.Args()
+	if len(args) >= 1 {
+		for _, value := range args {
+			if value == "help" {
+				displayHelp()
+				break;
+			}
+		}
+	}
 
-	traverse(*root, *filter)
-
-	//fmt.Println("duration: ", time.Now().Sub(reqStartTime).Seconds())
-	fmt.Println("duration: ", time.Now().Sub(reqStartTime).Nanoseconds())
-	
+	var MULTICORE int = runtime.NumCPU()
+	runtime.GOMAXPROCS(MULTICORE)
+	if result := Traverse(*root, *filter); result != Success {
+		os.Exit(-1)
+	}
+	fmt.Println("duration: ", time.Now().Sub(beginTime).Seconds(), "s")
 }
 
 
 // 路径分隔符的问题 如 /dev/ + / + abc
-// 路径是文件， 不是目录
-// 路径不存在
-// 目录下没有子目录或子文件
